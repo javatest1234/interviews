0c607565410dd45d8a9d9764e434103d431942e8
javatest1234 <vasupaduru@gmail.com>
2016-10-06 11:09:33 -0400

    Update Exercise2.java

--- java/src/main/java/com/elsevier/education/Exercise2.java
+++ java/src/main/java/com/elsevier/education/Exercise2.java
@@ -2,9 +2,6 @@ package com.elsevier.education;
 
 /**
 
-TODO refactor the Car to use dependency injection of the engine
-TODO allow use of either a gas engine or electric engine. 
-TODO make sure we have no-op implementations of both engines.
 
 */
 public class Exercise2 {
@@ -12,6 +9,7 @@ public class Exercise2 {
 	public static class Car {
 		
 		private GasEngine engine = new GasEngine();
+		private ElectricEngine eengine = new ElectricEngine();
 		
 		public Car() {
 		}
@@ -19,9 +17,33 @@ public class Exercise2 {
 		public void moveForward() {
 			engine.spinWheels();
 		}
+		
+		//refer the method spinWheels() based on type of engine.
+		
+		public GasEngine getGengine() {
+			return gengine;
+		}
+
+		public void setGengine(GasEngine gengine) {
+			this.gengine = gengine;
+		}
+
+		public ElectricEngine getEengine() {
+			return eengine;
+		}
+
+		public void setEengine(ElectricEngine eengine) {
+			this.eengine = eengine;
+		}
+		
 	}
 	
 	public static class GasEngine {
+		public void spinWheels() {
+			// no-op for now
+		}
+	}
+	public static class ElectricEngine {
 		public void spinWheels() {
 			// no-op for now
 		}
